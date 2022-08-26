d5e7d9705d6df2c5a0d0114f96688ed5a70a5cd7
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-08-26 13:53:34 +0200

    map reads updated

--- Scripts/01_Map_reads_BWA.sh
+++ Scripts/01_Map_reads_BWA.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH -A snic2021-5-8
+#SBATCH -A snic2022-5-242
+#SBATCH --array=1-4:1
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -M rackham
-#SBATCH -t 10-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH -J mapping
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ashley.sendell-price@imbim.uu.se
