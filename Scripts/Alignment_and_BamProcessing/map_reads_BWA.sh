82bdc09b692157183ba48ad9aa9cba4f97974042
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-09-05 13:34:02 +0200

    updated

--- Scripts/Alignment_and_BamProcessing/map_reads_BWA.sh
+++ Scripts/Alignment_and_BamProcessing/map_reads_BWA.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -M rackham
-#SBATCH -t 10:00:00
+#SBATCH -t 15:00:00
 #SBATCH -J mapping
 
 #Load required modules
@@ -19,7 +19,7 @@ fi
 #Move into mapping directory
 cd mapping
 
-#Set paths to reference fasta, sample list and file specifying paths to dirctories containing demultipled reads
+#Set paths to reference fasta, sample list and file specifying paths to dirctories containing demultiplexed reads
 REFGENOME=/crex/proj/snic2020-2-19/private/herring/assembly/Ch_v2.0.2.fasta
 SAMPLE_LIST=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/resources/sample.list.txt
 DEMULTIPLED_READs=/crex/proj/snic2020-2-19/private/herring/users/mats/uppstore2017191/low_pass_de_multiplexing/
@@ -30,10 +30,10 @@ SAMPLE_ID=$(head -n $SLURM_ARRAY_TASK_ID $SAMPLE_LIST | tail -n 1)
 R1=${DEMULTIPLED_READs}*/*/*${SAMPLE_ID}_R1.fastq.gz
 R2=${DEMULTIPLED_READs}*/*/*${SAMPLE_ID}_R2.fastq.gz
 if [ -e $R1 ] && [ -e $R2 ]; then
-    #bwa mem -t 2 -R "@RG\tID:$SAMPLE_ID\tSM:$SAMPLE_ID" $REFGENOME $R1 $R2 > ${SAMPLE_ID}.sam  
-    #samtools view -@ 2 -b -S -o ${SAMPLE_ID}.bam ${SAMPLE_ID}.sam && rm ${SAMPLE_ID}.sam
-    #samtools sort -@ 2 -o ${SAMPLE_ID}.sort.bam ${SAMPLE_ID}.bam && rm ${SAMPLE_ID}.bam
-    #samtools index -@ 2 ${SAMPLE_ID}.sort.bam
-    #samtools stats -@ 2 ${SAMPLE_ID}.sort.bam > ${SAMPLE_ID}.stat
+    bwa mem -t 2 -R "@RG\tID:$SAMPLE_ID\tSM:$SAMPLE_ID" $REFGENOME $R1 $R2 > ${SAMPLE_ID}.sam  
+    samtools view -@ 2 -b -S -o ${SAMPLE_ID}.bam ${SAMPLE_ID}.sam && rm ${SAMPLE_ID}.sam
+    samtools sort -@ 2 -o ${SAMPLE_ID}.sort.bam ${SAMPLE_ID}.bam && rm ${SAMPLE_ID}.bam
+    samtools index -@ 2 ${SAMPLE_ID}.sort.bam
+    samtools stats -@ 2 ${SAMPLE_ID}.sort.bam > ${SAMPLE_ID}.stat
     samtools depth ${SAMPLE_ID}.sort.bam | awk '{sum+=$3} END { print sum/NR}' > ${SAMPLE_ID}.depth
 fi
--- Scripts/Alignment_and_BamProcessing/simulate_lowdepth_from_highdepth.sh
+++ Scripts/Alignment_and_BamProcessing/simulate_lowdepth_from_highdepth.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -M snowy
-#SBATCH -t 5-00:00:00
+#SBATCH -t 2-00:00:00
 #SBATCH -J DownsampleBams
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=ashley.sendell-price@imbim.uu.se
@@ -11,27 +11,35 @@
 #Load required modules
 ml bioinfo-tools samtools
 
-#Set desired depth of coverage
-REQUIRED_DEPTH=1
+#Create directory for simulated low pass data
+if [[ ! -d simulated_low_pass ]]
+then
+    mkdir simulated_low_pass
+fi
+
+#Set desired depth of coverage based on average coverage of the 944 low pass samples
+TARGET_DEPTH=$(cat ../mapping/*.depth | awk '{sum+=$1} END { print sum/NR}')
+echo $TARGET_DEPTH > target.depth
 
 #For each of the high depth bams do the following ...
 for BAM in $(ls /proj/snic2020-2-19/private/herring/alignment/79_individuals/*.bam)
 do
-#Get ID from bam file name
-SAMPLE_ID=$(basename $BAM | cut -d "." -f 1)
-#Calculate average depth of coverage for bam
-MEAN_DEPTH=$(samtools depth -a $BAM | awk '{sum+=$3} END {print sum/NR}')
-#Calculate proportion of reads to keep to acheive desired depth of coverage
-PROP_RETAIN=$(awk "BEGIN {print $REQUIRED_DEPTH/$MEAN_DEPTH}")
-#Round proportion to 3 decimal places
-PROP_RETAIN_3D=$(printf '%.*f\n' 3 $PROP_RETAIN)
-#Downsample bam file
-samtools view -s $PROP_RETAIN_3D -b $BAM > subsampled_${REQUIRED_DEPTH}X.${SAMPLE_ID}.bam
-#Sort bam file
-samtools sort subsampled_${REQUIRED_DEPTH}X.${SAMPLE_ID}.bam -o subsampled_${REQUIRED_DEPTH}X.${SAMPLE_ID}.sorted.bam
-#Remove un-sorted bam
-rm subsampled_${REQUIRED_DEPTH}X.${SAMPLE_ID}.bam
+    #Get ID from bam file name
+    SAMPLE_ID=$(basename $BAM | cut -d "." -f 1)
+    #Calculate average depth of coverage for bam
+    MEAN_DEPTH=$(samtools depth -a $BAM | awk '{sum+=$3} END {print sum/NR}')
+    #Calculate proportion of reads to keep to acheive desired depth of coverage
+    PROP_RETAIN=$(awk "BEGIN {print $TARGET_DEPTH/$MEAN_DEPTH}")
+    #Round proportion to 3 decimal places
+    PROP_RETAIN_3D=$(printf '%.*f\n' 3 $PROP_RETAIN)
+    #Downsample bam file
+    samtools view -s $PROP_RETAIN_3D -b $BAM > subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.bam
+    #Sort bam file
+    samtools sort subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.bam -o subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.sort.bam
+    #Remove un-sorted bam
+    rm subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.bam
+    #Index bam file and output stats
+    samtools index -@ 2 subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.sort.bam
+    samtools stats -@ 2 subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.sort.bam > subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.stat
+    samtools depth subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.sort.bam | awk '{sum+=$3} END { print sum/NR}' > subsampled_${TARGET_DEPTH}X.${SAMPLE_ID}.depth
 done
-
-
-
