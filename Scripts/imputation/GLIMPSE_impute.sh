fa733152aacdfa038124b95fc56d3bc36fea87b4
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-09-06 15:48:19 +0200

    updates

--- Scripts/imputation/GLIMPSE_impute.sh
+++ Scripts/imputation/GLIMPSE_impute.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -M rackham
-#SBATCH -t 1-00:00:00
+#SBATCH -t 2-00:00:00
 #SBATCH -J Genotype_Imputation
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=ashley.sendell-price@imbim.uu.se
@@ -14,12 +14,19 @@
 #STEP 1: Define paths to ANGSD, BAGLE, GLIMPSE and load required modules
 ######################################################################################
 
+#Load conda environment
+ml conda
+source conda_init.sh
+#conda create -y -p /home/ashle/conda_envs/impute_genos
+conda activate /home/ashle/conda_envs/impute_genos
+#conda install whatshap nomkl
+
+#Load other required modules and set path to GLIMPSE directory
 ml bioinfo-tools ABINIT/8.10.3 GCCcore/8.3.0 bcftools/1.10 samtools vcftools
-BEAGLE4=/proj/snic2020-2-19/private/herring/users/ashsendell/BIN/beagle.27Jan18.7e1.jar
 GLIMPSE_DIR=/crex/proj/snic2020-2-19/private/darwins_finches/users/erikenbody/Finch_lowpass/tools/GLIMPSE_1.10/GLIMPSE
 
 ######################################################################################
-#STEP 2: Determine chromosome
+#STEP 2: Determine chromosome using slurm array job id
 ######################################################################################
 
 ChrName=chr${SLURM_ARRAY_TASK_ID}
@@ -49,16 +56,28 @@ cd $ChrName
 mkdir reference
 cd reference
 
-#Extract polymorphic sites from Mafalda's 91 indv high depth VCF
+#Create a list of 79 high depth sample names
+for BAM in $(ls /proj/snic2020-2-19/private/herring/alignment/79_individuals/*.bam)
+do
+    basename $BAM | sed "s/.MD.RG.bam//g" >> high.depth.samples
+done
+
+#Extract polymorphic sites from Mafalda's 91 indv high depth VCF keeping only samples in the list
+#we just created
 vcftools --gzvcf /proj/snic2020-2-19/private/herring/users/mafalda/variants/91_indv/vcfs_by_chromosomes_F2/herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.inv.${ChrName}.vcf.gz \
---min-alleles 2 --max-alleles 2 --remove-indels \
---out herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.variantsOnly.${ChrName} \
+--min-alleles 2 --max-alleles 2 --remove-indels --keep high.depth.samples \
+--out ${ChrName}.79_high_depth_samples \
 --recode
 
-#Phase and index VCF file - this will be used as the reference panel
-java -Xmx15000m -jar $BEAGLE4 \
-gt=herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.variantsOnly.${ChrName}.recode.vcf \
-out=herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.variantsOnly.${ChrName}.phased
+#Phase VCF file using whatshap
+BAMS=$(ls /proj/snic2020-2-19/private/herring/alignment/79_individuals/*.bam | tr "\n" " ")
+whatshap phase -o ${ChrName}.79_high_depth_samples.phased.vcf \
+--reference=${REFGEN} ${ChrName}.79_high_depth_samples.recode.vcf $BAMS
+
+
+
+
+
 
 #Remove un-phased VCF and index phased VCF using tabix
 rm herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.variantsOnly.${ChrName}.recode.vcf
@@ -94,10 +113,9 @@ cd genotype_likelihoods
 #Compute GLs from low pass bams
 for BAM in $(ls ../../chrom_bams/${ChrName}/*.bam)
 do
-SAMPLE_ID=$(basename $BAM | cut -d "." -f 2)
+SAMPLE_ID=$(basename $BAM | sed "s/${ChrName}.//g" | sed "s/.sort.bam//g")
 VCF=../reference/herring_sentieon_91ind_190521.SV.VF.F2.maxDPtriple.setGT.variantsOnly.${ChrName}.phased.vcf.gz
 TSV=../reference/${ChrName}.sites.tsv.gz
-REFGEN=/proj/snic2020-2-19/private/herring/assembly/Ch_v2.0.2.fasta
 OUT=${ChrName}.${SAMPLE_ID}.vcf.gz
 bcftools mpileup -f ${REFGEN} -I -E -a 'FORMAT/DP' -T ${VCF} -r ${ChrName} ${BAM} -Ou | bcftools call -Aim -C alleles -T ${TSV} -Oz -o ${OUT}
 
---
+++ Scripts/imputation/whatshap_phase.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -l
+
+#SBATCH --array=1-1:1
+#SBATCH -A snic2022-5-242
+#SBATCH -p node
+#SBATCH -n 1
+#SBATCH -M rackham
+#SBATCH -t 5:00:00
+#SBATCH -J whatshapp_phase
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=ashley.sendell-price@imbim.uu.se
+
+#Load conda environment
+ml conda
+source conda_init.sh
+#conda create -y -p /home/ashle/conda_envs/impute_genos
+conda activate /home/ashle/conda_envs/impute_genos
+#conda install whatshap nomkl
+
+#Load other required modules and set path to GLIMPSE directory
+ml bioinfo-tools GATK/4.1.1.0 bcftools/1.10
+
+#Set chromosome name and create / move into chromosome directory
+ChrName=chr${SLURM_ARRAY_TASK_ID}
+
+#Create / move into directory "GLIMPSE_imputation"
+if [[ ! -d GLIMPSE_imputation ]]
+then
+    mkdir GLIMPSE_imputation
+    cd GLIMPSE_imputation
+else
+    cd GLIMPSE_imputation
+fi
+
+#Make directory for chromosome and a subdirectory called reference
+mkdir ${ChrName}
+mkdir ${ChrName}/reference
+
+#Move into subdirectory
+cd ${ChrName}/reference
+
+#Combine sample GVCFs using GATK CombineGVCFs
+REFGENOME=/proj/snic2020-2-19/private/herring/assembly/Ch_v2.0.2.fasta
+GVCFs=$(ls /proj/snic2020-2-19/private/herring/variants/herring_79individuals/*/*.g.vcf.gz | sed -e 's/^/--variant /' | tr "\n" " " )
+gatk CombineGVCFs -R $REFGENOME $GVCFs --intervals $ChrName -O ${ChrName}.herring_79individuals.g.vcf.gz
+
+#Call sample genotypes using GATK GenotypeGVCFs
+gatk --java-options "-Xmx4g" GenotypeGVCFs -R $REFGENOME \
+-V ${ChrName}.herring_79individuals.g.vcf.gz -O ${ChrName}.herring_79individuals.vcf.gz
