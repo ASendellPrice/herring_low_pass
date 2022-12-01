c9a1826d5bc39a8f6801cfc29f6556b806e6958f
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-12-01 13:39:50 +0100

    added VCF annotation script

--- Scripts/Genotype_likelihoods/annotate_VCF_snpEff.sh
+++ Scripts/Genotype_likelihoods/annotate_VCF_snpEff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
 
+#SBATCH --array=1-26:1
 #SBATCH -A snic2022-5-242
 #SBATCH -p core -n 1
 #SBATCH -M rackham
@@ -7,7 +8,6 @@
 #SBATCH -J snpEff
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=ashley.sendell-price@imbim.uu.se
-
 
 ######################################################################################################
 # NOTES: HOW TO BUILD A SNPEFF DATABASE
@@ -36,33 +36,35 @@
 #java -jar snpEff.jar build -gtf22 -v Ch_v2.0.2
 ######################################################################################################
 
-
 #Create directory for analysis and move into it
-mkdir snpEff_annotation
-cd snpEff_annotation
+if [[ ! -d snpEff_annotation ]]
+then
+    mkdir snpEff_annotation
+    cd snpEff_annotation
+else
+    cd snpEff_annotation
+fi
+
+#STEP 2: Determine chromosome
+ChrName=chr${SLURM_ARRAY_TASK_ID}
 
 #Set path to snpEff and snpSift jar files and perl file for extracting effects from annotated vcf
 snpEff_jar=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/bin/snpEff/snpEff.jar
 snpSift_jar=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/bin/snpEff/SnpSift.jar
 snpEff_perl=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/bin/snpEff/scripts/vcfEffOnePerLine.pl
 
-#For each chromosome do the following
-for CHROM in $(seq 1 26)
-do
-    #Set path to angsd outputted VCF file
-    CHROM_VCF=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/genotype_likelihoods/chr${CHROM}/HerringLowPass_GATKMethod_MinMAF0.01_chr${CHROM}_updatedIDs.vcf.gz
+#Set path to angsd outputted VCF file
+CHROM_VCF=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/genotype_likelihoods/${ChrName}/HerringLowPass_GATKMethod_MinMAF0.01_${ChrName}_updatedIDs.vcf.gz
 
-    #Run snpEff (set upstream and downstream interval size to zero using -ud 0)
-    java -jar $snpEff_jar Ch_v2.0.2 $CHROM_VCF -ud 0 > temp.annotated.vcf
+#Run snpEff (set upstream and downstream interval size to zero using -ud 0)
+java -jar $snpEff_jar Ch_v2.0.2 $CHROM_VCF -ud 0 > ${ChrName}.temp.annotated.vcf
 
-    #Extract annotations from VCF
-    #If multiple effects one will be printed per line
-    cat temp.annotated.vcf | $snpEff_perl \
-    | java -jar $snpSift_jar extractFields - CHROM POS REF ALT "ANN[*].EFFECT" "ANN[*].IMPACT" "ANN[*].GENE" \
-    | uniq \
-    > chr${CHROM}_variant_effects.snpEff.txt
+#Extract annotations from VCF
+#If multiple effects one will be printed per line
+cat ${ChrName}.temp.annotated.vcf | $snpEff_perl \
+| java -jar $snpSift_jar extractFields - CHROM POS REF ALT "ANN[*].EFFECT" "ANN[*].IMPACT" "ANN[*].GENE" \
+| uniq \
+> ${ChrName}_variant_effects.snpEff.txt
 
-    #Remove temporary files
-    rm temp.annotated.vcf
-done 
-
+#Remove temporary files
+rm ${ChrName}.temp.annotated.vcf
