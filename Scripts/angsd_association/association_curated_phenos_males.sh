5383c70fa12ac0eca4108ca5c5bf867a4d1fe240
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-12-06 11:43:58 +0100

    updates female gwas

--- Scripts/angsd_association/association_curated_phenos_males.sh
+++ Scripts/angsd_association/association_curated_phenos_males.sh
@@ -30,12 +30,37 @@ mkdir $ChrName
 cd $ChrName
 
 #Set path to VCF file and curated phenotypes file
-VCF=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/genotype_likelihoods/${ChrName}/HerringLowPass_GATKMethod_MinMAF0.05_${ChrName}_updatedIDs.vcf.gz
+VCF=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/genotype_likelihoods/${ChrName}/HerringLowPass_GATKMethod_MinMAF0.01_${ChrName}_updatedIDs.vcf.gz
 PHENOS=/proj/snic2020-2-19/private/herring/users/ashsendell/herring_low_pass/resources/curated_phenos_males.input.txt
 
 #Get basename for VCF and PHENOS
 VCF_BASE=$(basename $VCF)
 PHENOS_BASE=$(basename $PHENOS)
+
+# Curated phenotypes file for both males and females is structrued as below. Where
+# any missing data is denoted by "NA". This file uses the same file format as SNPTEST
+# (https://mathgen.stats.ox.ac.uk/genetics_software/snptest/snptest.html). This is a 
+# space-separated file with two header lines followed by data, as follows:
+# Header line: This line lists the name of each column.
+# Column type line: This line lists the 'type' of each column:
+#     - 0 for the sample ID column
+#     - D for a column containing discrete values
+#     - P or C for columns containing continuous values (each value must be numerical, or a missing value).
+#     - B for a column containing a binary trait (e.g. Male or Female - coded 1 or 2)
+# Data lines: Data for each sample. There should be one data line per sample in the genotypes file,
+# these must be in the same order that they appear in the genotypes file.
+
+# |---------------------------------------------------------|
+# | ID            age	maturity    gonad_weight    ID_2    | <- Header line
+# | 0	          D	    D	        P	            D       | <- Column type line
+# | Hastkar_24	  6	    5	        10.21	        24      | <- Data lines
+# | Hastkar_53	  5	    6	        14.87	        53      |
+# | Hastkar_103	  5	    5	        19.58	        103     |
+# | Hastkar_501	  5	    4	        1.77	        501     |
+# | Hastkar_502	  6	    5	        9.33	        502     |
+# | Hastkar_503	  6	    5	        9.41	        503     |
+# | Hastkar_504	  8	    5	        12.06	        504     |
+# |---------------------------------------------------------|
 
 #From curated phenotypes file extract list of sample IDs:
 tail -n +3 $PHENOS | cut -f 1 > phenotyped_samples.IDs.txt
@@ -44,8 +69,8 @@ tail -n +3 $PHENOS | cut -f 1 > phenotyped_samples.IDs.txt
 zcat $VCF \
 | sed 's/VCFv4.2(angsd version)/VCFv4.2/g' \
 | vcftools --vcf - --keep phenotyped_samples.IDs.txt --recode --stdout \
-| bgzip > Subset_${VCF_BASE} # <- can angsd pass gzipped vcfs???
-tabix Subset_${VCF_BASE}
+| bgzip > Subset_${VCF_BASE}
+tabix Subset_${VCF_BASE} -f
 
 #Get sample order in the VCF file
 bcftools query -l Subset_${VCF_BASE} \
@@ -73,3 +98,12 @@ $ANGSD \
 -doAsso 4 -nInd $N_INDV -doMaf 4 \
 -out ${ChrName}_association_male_gonad_weight
 
+#Perform association study on maturity (age = covariate)
+$ANGSD \
+-vcf-gp Subset_${VCF_BASE} \
+-fai /proj/snic2020-2-19/private/herring/assembly/Ch_v2.0.2.fasta.fai \
+-sampleFile Subset_${PHENOS_BASE} \
+-whichPhe gonad_weight \
+-whichCov age \
+-doAsso 4 -nInd $N_INDV -doMaf 4 \
+-out ${ChrName}_association_male_gonad_weight
